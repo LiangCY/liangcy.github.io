08d35e8b6ecb1cf0a25cba6a5a7796e060046230
LiangCY <liangcynju@qq.com>
2017-03-24 15:23:56 +0800

    nothing

--- js/app.js
+++ js/app.js
@@ -12,3 +12,35 @@ $('#days').text(days);
 //     console.log('添加数据失败，返回错误信息：' + error.description);
 //   }
 // });
+
+var bucketUrl = 'http://gallery.gz.bcebos.com/';
+var config = {
+  endpoint: 'http://gz.bcebos.com',
+  credentials: {
+    ak: '733769b45b7b4ac5ae5128f2692c5911',           //您的AccessKey
+    sk: 'dd07bb2d28d44a9d9bd9671cc60b927e'            //您的SecretAccessKey
+  }
+};
+var bucket = 'gallery';
+var client = new baidubce.sdk.BosClient(config);
+
+
+client.listObjects(bucket)
+  .then(function (response) {
+    var contents = response.body.contents;
+    if (contents.length < 9) {
+      var rand = Math.floor(Math.random() * contents.length);
+      var file = contents[rand];
+      $('body').css('background-image', "url(" + bucketUrl + file.key + ")");
+    } else {
+      for (var i = 1; i <= 9; i++) {
+        rand = Math.floor(Math.random() * contents.length);
+        var filename = contents.splice(rand, 1)[0].key;
+        console.log(filename);
+        $('.grid.g-' + i).css('background-image', "url(" + bucketUrl + filename + ")");
+      }
+    }
+  })
+  .catch(function (error) {
+    // 查询失败
+  });
--- js/upload.js
+++ js/upload.js
@@ -1,16 +1,34 @@
+var config = {
+  endpoint: 'http://gz.bcebos.com',
+  credentials: {
+    ak: '733769b45b7b4ac5ae5128f2692c5911',           //您的AccessKey
+    sk: 'dd07bb2d28d44a9d9bd9671cc60b927e'            //您的SecretAccessKey
+  }
+};
+var client = new baidubce.sdk.BosClient(config);
+
 var $uploadButton = $('#uploadButton');
+var $progress = $('#progress');
 $uploadButton.on('click', function () {
   var uploadFile = $("#uploadFile")[0].files[0];
   if (uploadFile) {
     var originalName = uploadFile.name;
     var dotIndex = originalName.lastIndexOf('.');
-    var extention = originalName.slice(dotIndex);
-    var name = originalName.slice(0, dotIndex) + '_' + Date.now().toString(36) + extention;
-    var file = new Bmob.File(name, uploadFile);
-    file.save().then(function (obj) {
-      console.log(obj.url());
-    }, function (error) {
-      console.log(error);
+    var ext = originalName.slice(dotIndex);
+    var key = originalName.slice(0, dotIndex) + '_' + Date.now().toString(36) + ext;
+    var bucket = 'gallery';
+    client.putObjectFromBlob(bucket, key, uploadFile)
+      .then(function (response) {
+        $progress.text('上传成功')
+      })
+      .catch(function (error) {
+        $progress.text(error)
+      });
+    client.on('progress', function (e) {
+      if (e.lengthComputable) {
+        var percentage = (e.loaded / e.total) * 100;
+        $progress.text('上传中，已上传了' + percentage.toFixed(2) + '%')
+      }
     });
   }
 });
